6972f4b32a1b85f4765be56396e582411f9d2453
Timo Tijhof <krinklemail@gmail.com>
2018-03-08 18:50:27 -0800

    wm-ecmabot: Re-enable js-eval

--- wm-ecmabot.js
+++ wm-ecmabot.js
@@ -3,7 +3,7 @@ var path = require('path');
 var util = require("util");
 var http = require("http");
 
-//var Sandbox = require("./lib/sandbox");
+var Sandbox = require("./lib/sandbox");
 
 var Bot = require("./lib/irc");
 var Shared = require("./shared");
@@ -12,8 +12,8 @@ var scrapeMdn = require('scrape-mdn');
 
 var JSBot = function(profile) {
 	// PATCH(wm-ecmabot): Only V8-based for now. --Krinkle
-	//this.sandbox = new Sandbox(path.join(__dirname, "ecmabot-utils.js"));
-	//this.executeRegex = /^(v8?>|>>>?)(.*)/;
+	this.sandbox = new Sandbox(path.join(__dirname, "ecmabot-utils.js"));
+	this.executeRegex = /^(v8?>|>>>?)(.*)/;
 
 	Bot.call(this, profile);
 	this.set_log_level(this.LOG_ALL);
@@ -27,10 +27,7 @@ util.inherits(JSBot, Bot);
 JSBot.prototype.init = function() {
 	Bot.prototype.init.call(this);
 
-	// TODO: The v8/shovel doesn't work anymore. The new docker-run
-	// based approach is nice and works, but needs work to work
-	// on Wikimedia Toolforge. Disable for now.
-	// this.register_listener(this.executeRegex, this.execute_js);
+	this.register_listener(this.executeRegex, this.execute_js);
 
 	this.register_command("mdn", this.mdn, {
 		help: "Search the Mozilla Developer Network. "
